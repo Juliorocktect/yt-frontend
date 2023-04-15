18d678558ac77139c9fff84481224c14309d2c73
Juliorocktect <113830349+Juliorocktect@users.noreply.github.com>
2023-04-15 20:58:19 +0200

    implementation of regeastration

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
 function Register() {
   var strengthbar = document.getElementById("strengthbar");
   var display = document.getElementById("display");
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUsername] = useState("");
+  const [pictureUrl, setPictureUrl] = useState("");
+  const [password, setPassWord] = useState("");
+  const [passwordTwo, setPassWordTwo] = useState("");
 
   function checkpassword(password) {
     var strength = 0;
@@ -50,34 +59,198 @@ function Register() {
         break;
     }
   }
-  <body></body>;
+
+  function handle(e) {
+    if (e.key === "Enter") {
+      action();
+    }
+    return false;
+  }
+  function action() {
+    let params = new URLSearchParams("");
+    params.set("firstName", firstName);
+    params.set("lastName", lastName);
+    params.set("userName", userName);
+    params.set("pictureUrl", pictureUrl);
+    params.set("passWord", password);
+    let Url = "http://localhost:8080/user/create?" + params.toString();
+
+    var requestOptions = {
+      method: "POST",
+      redirect: "follow",
+    };
+
+    fetch(Url, requestOptions)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          navigate("/");
+        }
+        return response.text();
+      })
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
 
   return (
     <>
-      <form>
-        <div id="username">
-          <input type="text" placeholder="username" id="input" />
+      <div className="main">
+        <div className="background"></div>
+        <div className="login-component " id="login-component">
+          <h1>Login</h1>
+          <div className="email-container">
+            <form
+              onSubmit={
+                (action,
+                (event) => {
+                  event.preventDefault();
+                })
+              }
+            >
+              <input
+                type="text"
+                id="username-input"
+                placeholder="  firstname"
+                className="input"
+                onChange={(val) => {
+                  setFirstName(val.target.value);
+                }}
+                onKeyDown={handle}
+              />
+              <FaUserAlt className="icon" />
+            </form>
+          </div>
+          <div className="password-container">
+            <form
+              onSubmit={
+                (action,
+                (event) => {
+                  event.preventDefault();
+                })
+              }
+            >
+              <input
+                type="text"
+                id="password-input"
+                placeholder="  lastname"
+                className="input"
+                onChange={(val) => {
+                  setLastName(val.target.value);
+                }}
+                onKeyDown={handle}
+              />
+              <FaLock className="icon" />
+            </form>
+          </div>
+          <div className="email-container">
+            <form
+              onSubmit={
+                (action,
+                (event) => {
+                  event.preventDefault();
+                })
+              }
+            >
+              <input
+                type="text"
+                id="username-input"
+                placeholder="  username"
+                className="input"
+                onChange={(val) => {
+                  setUsername(val.target.value);
+                }}
+                onKeyDown={handle}
+              />
+              <FaUserAlt className="icon" />
+            </form>
+          </div>
+          <div className="email-container">
+            <form
+              onSubmit={
+                (action,
+                (event) => {
+                  event.preventDefault();
+                })
+              }
+            >
+              <input
+                type="text"
+                id="username-input"
+                placeholder="  picture url"
+                className="input"
+                onChange={(val) => {
+                  setPictureUrl(val.target.value);
+                }}
+                onKeyDown={handle}
+              />
+              <FaUserAlt className="icon" />
+            </form>
+          </div>
+          <div className="password-container">
+            <form
+              onSubmit={
+                (action,
+                (event) => {
+                  event.preventDefault();
+                })
+              }
+            >
+              <input
+                type="password"
+                id="password-input"
+                placeholder="  password"
+                className="input"
+                onChange={(val) => {
+                  setPassWord(val.target.value);
+                }}
+                onKeyDown={handle}
+              />
+              <FaLock className="icon" />
+            </form>
+          </div>
+          <div className="password-container">
+            <form
+              onSubmit={
+                (action,
+                (event) => {
+                  event.preventDefault();
+                })
+              }
+            >
+              <input
+                type="password"
+                id="password-input"
+                placeholder="  repeat password"
+                className="input"
+                onChange={(val) => {
+                  setPassWordTwo(val.target.value);
+                }}
+                onKeyDown={handle}
+              />
+              <FaLock className="icon" />
+            </form>
+          </div>
+          <div className="options"></div>
+          <form
+            onSubmit={
+              (action,
+              (event) => {
+                event.preventDefault();
+              })
+            }
+          >
+            <button onClick={action} id="login">
+              Login
+            </button>
+          </form>
+          <div id="bottom">
+            Don't have an account?
+            <br />
+            <a href="/register">Register</a>
+            <span></span>
+          </div>
         </div>
-        <div id="pass">
-          <input type="text" placeholder="password" id="input" />
-        </div>
-        <div id="passtwo">
-          <input type="text" placeholder="repeat password" id="input" />
-        </div>
-        <div id="strengthbar"></div>
-        <div id="display"></div>
-        <div id="choosePic">
-          <input
-            type="file"
-            name=""
-            id="file-input"
-            placeholder="Upload profile-picture"
-          />
-        </div>
-        <div>
-          <button id="submitBtn">Sign Up</button>
-        </div>
-      </form>
+      </div>
     </>
   );
 }
